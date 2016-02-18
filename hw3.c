688614df36351d4f56b15a0fcf122521d0953ddb
Jswizzy <smithjustinn@hotmail.com>
2016-02-18 12:31:22 -0500

    hi

--- hw3.c
+++ hw3.c
@@ -10,6 +10,8 @@ can additional libraries be included.
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <sys/fcntl.h>
+#include <sys/signal.h>
 #include "shell.h"
 
 /*
@@ -19,37 +21,52 @@ can additional libraries be included.
                 argv = array of argument values
 -----------------------------------
 */
+static void sig_child_handler(int sig);
 
-int main( int argc, char** argv ) {
-  command_t cmd;
-  char line[MAXSTRLEN];
-  char fullpath[MAXSTRLEN];
-  int done = FALSE;
+int main(int argc, char **argv) {
+    command_t cmd;
+    char line[MAXSTRLEN];
+    char fullpath[MAXSTRLEN];
+    int done = FALSE;
 
-  while (!done) {
-    printf(">> ");
-    fgets(line, MAXSTRLEN, stdin);
-    line[my_strlen(line)-1] = '\0'; // get rid of newline
-    parse(line, &cmd);
+    if (signal(SIGCHLD, sig_child_handler) == SIG_ERR) {
 
-    if (cmd.argc > 0) {   // fix for no command (ie white space) entered
-      if (my_strequal(cmd.name, "exit")) {
-	done = TRUE;
-      }
-      else if (is_builtin(&cmd)) {
-	do_builtin(&cmd);
-      }
-      else if (find_fullpath(fullpath, &cmd)) {
-	// NOTE: find_fullpath() is called again in execute
-	execute(&cmd);
-      }
-      else {
-	printf("invalid command\n");
-      }
+        perror("Unable to create new child signal handler!");
+        exit(-1);
+
     }
-    
-    cleanup(&cmd);
-  }
-  
-  return 0;
+
+    while (!done) {
+        printf(">> ");
+        fgets(line, MAXSTRLEN, stdin);
+        line[my_strlen(line) - 1] = '\0'; // get rid of newline
+        parse(line, &cmd);
+
+        if (cmd.argc > 0) {   // fix for no command (ie white space) entered
+            if (my_strequal(cmd.name, "exit")) {
+                done = TRUE;
+            }
+            else if (is_builtin(&cmd)) {
+                do_builtin(&cmd);
+            }
+            else if (find_fullpath(fullpath, &cmd)) {
+                // NOTE: find_fullpath() is called again in execute
+                execute(&cmd);
+            }
+            else {
+                printf("invalid command\n");
+            }
+        }
+
+        cleanup(&cmd);
+    }
+
+    return 0;
 } // end main function
+
+void sig_child_handler(int sig) {
+
+    int status;
+    wait (&status);
+
+}
